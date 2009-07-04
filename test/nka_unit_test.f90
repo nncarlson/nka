a38a98dc3cb569ba10b3a98a65c0ca1280c93044
Neil Carlson <neil.n.carlson@gmail.com>
2009-07-04 17:41:50 +0000

    Updates to match new C version.

--- test/nka_unit_test.f90
+++ test/nka_unit_test.f90
@@ -1,10 +1,10 @@
 !!
-!!  NKA_UNIT_TEST
+!!  NKA_F95_TEST
 !!
 !!  Unit test program for the NONLINEAR_KRYLOV_ACCELERATOR module.
 !!
-!!  Neil N. Carlson <nnc@newmexico.com> 15 Feb 2004
-!!  Last revised 14 Oct 2006
+!!  Neil N. Carlson <neil.n.carlson@gmail.com> 15 Feb 2004
+!!  Last revised 4 Jul 2009
 !!
 !!  This program exercises the nonlinear Krylov acceleration procedure
 !!  implemented by the NONLINEAR_KRYLOV_ACCELERATOR module by applying it to
@@ -27,104 +27,128 @@
 !!  l2 norm of r(u), the reduction factor in the norm, and the net convergence
 !!  rate are printed for each iteration.
 !!
+!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+!!
+!! Copyright (c) 2004, 2009  Neil N. Carlson
+!!
+!! Permission is hereby granted, free of charge, to any person obtaining a
+!! copy of this software and associated documentation files (the "Software"),
+!! to deal in the Software without restriction, including without limitation
+!! the rights to use, copy, modify, merge, publish, distribute, sublicense,
+!! and/or sell copies of the Software, and to permit persons to whom the
+!! Software is furnished to do so, subject to the following conditions:
+!!
+!! The above copyright notice and this permission notice shall be included
+!! in all copies or substantial portions of the Software.
+!!
+!! THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+!! IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+!! FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+!! THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+!! LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+!! FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+!! DEALINGS IN THE SOFTWARE.
+!!
 
-program nka_unit_test
+program nka_f95_test
 
-use nonlinear_krylov_accelerator
-implicit none
+  use nonlinear_krylov_accelerator
+  implicit none
 
-integer, parameter :: dp = kind(1.0d0)
-integer, parameter :: N = 50
-real(kind=dp), parameter :: A = 0.02_dp
+  integer,  parameter :: dp = kind(1.0d0)
+  integer,  parameter :: N = 50
+  real(dp), parameter :: A = 0.02_dp
 
-type :: system
-  integer :: nx, ny
-  type(nka_state) :: fpa
-  real(kind=dp) :: a
-  real(kind=dp), pointer :: ax(:,:) => null(), ay(:,:) => null(), ac(:,:) => null(), q(:,:) => null()
-end type system
+  type :: system
+    integer :: nx, ny
+    real(dp) :: a, hx, hy
+    real(dp), pointer :: ax(:,:) => null(), ay(:,:) => null(), ac(:,:) => null(), q(:,:) => null()
+    type(nka_state) :: nka
+  end type system
 
-type(system) :: sys
-real(kind=dp) :: u(N*N), h
+  type(system) :: sys
+  real(dp) :: u(N*N), h, omega
+  integer :: nsweep
 
-!! Initialize the system
-sys%nx = N
-sys%ny = N
-sys%a = A
-allocate(sys%ax(sys%nx+1,sys%ny), sys%ay(sys%nx,sys%ny+1), sys%ac(sys%nx,sys%ny), sys%q(sys%nx,sys%ny))
-call nka_create (sys%fpa, size(u), maxv=5)
-h = 1.0_dp / N
-sys%q = h**2
+  !! Initialize the system
+  sys%a = A
+  sys%nx = N
+  sys%ny = N
+  sys%hx = 1.0_dp / sys%nx
+  sys%hy = 1.0_dp / sys%ny
+  allocate(sys%ax(sys%nx+1,sys%ny), sys%ay(sys%nx,sys%ny+1), sys%ac(sys%nx,sys%ny))
+  allocate(sys%q(sys%nx,sys%ny))
+  call nka_create (sys%nka, size(u), maxv=5)
+  sys%q = sys%hx*sys%hy
 
-call solve (sys, u, nsweep=2, omega=1.4_dp, use_fpa=.true.)
-call solve (sys, u, nsweep=2, omega=1.4_dp, use_fpa=.false.)
+  nsweep = 2
+  omega = 1.4_dp
 
-call nka_destroy(sys%fpa)
+  call solve (sys, nsweep, omega, use_nka=.true., sol=u)
+  call solve (sys, nsweep, omega, use_nka=.false., sol=u)
+
+  call nka_destroy (sys%nka)
 
 contains
 
-  subroutine solve (sys, sol, nsweep, omega, use_fpa)
+  subroutine solve (sys, nsweep, omega, use_nka, sol)
 
     type(system), intent(inout) :: sys
-    real(kind=dp), intent(out) :: sol(:)
-    integer, intent(in) :: nsweep
-    real(kind=dp), intent(in) :: omega
-    logical, intent(in) :: use_fpa
+    integer,  intent(in)  :: nsweep
+    real(dp), intent(in)  :: omega
+    logical,  intent(in)  :: use_nka
+    real(dp), intent(out) :: sol(:)
 
     integer :: itr
-    real(kind=dp) :: rnorm, rnorm0, red, rate, rflat(sys%nx*sys%ny)
-    real(kind=dp), dimension(0:sys%nx+1,0:sys%ny+1), target :: upad, rpad
-    real(kind=dp), dimension(:,:), pointer :: u, r
+    real(dp) :: rnorm, rnorm0, red, rate
+    real(dp), target :: upad(0:sys%nx+1,0:sys%ny+1)
+    real(dp), pointer :: u(:,:)
+    real(dp) :: r(sys%nx*sys%ny)
+    integer, parameter :: MAXITR = 999
+    real(dp), parameter :: TOL = 1.0d-6
 
-    if (use_fpa) then
+    if (use_nka) then
       write(unit=*,fmt='(/,a,/)') 'ACCELERATED SOLVE'
     else
       write(unit=*,fmt='(/,a,/)') 'UNACCELERATED SOLVE'
     end if
     write(unit=*,fmt='(a4,a14,a13,a8)') 'Iter', 'Residual Norm', 'Reduction', 'Rate'
 
-    u => upad(1:sys%nx,1:sys%ny)
-    r => rpad(1:sys%nx,1:sys%ny)
+    upad = 0.0_dp  ! zero initial solution and zero value for ghost cells
+    u => upad(1:sys%nx,1:sys%ny)  ! the true part of the solution vector
 
-    upad = 0.0_dp  ! We start with a zero initial solution, and for the padded vectors,
-    rpad = 0.0_dp  ! we need to zero the values in the surrounding ghost cells.
+    call residual (sys, upad, r)
+    rnorm0 = l2norm(r)
+    write(*,'(i3,a,es14.6)') 0, ':', rnorm0
 
-    r = residual(sys, upad)
-    rnorm0 = norm(r)
-    write(unit=*,fmt='(i3,a,es14.6)') 0, ':', rnorm0
+    do itr = 1, MAXITR
+      call ssor_pc (sys, nsweep, omega, r)
+      if (use_nka) call nka_correction (sys%nka, r)
+      u = u - reshape(r,shape(u))
 
-    do itr = 1, 999
-      rpad = ssor(sys, nsweep, omega, rpad)
-      if (use_fpa) then
-        rflat = reshape(r, shape(rflat))
-        call nka_correction (sys%fpa, rflat)
-        r = reshape(rflat, shape(r))
-      end if
-      u = u - r
-
-      r = residual(sys, upad)
-      rnorm = norm(r)
+      call residual (sys, upad, r)
+      rnorm = l2norm(r)
       red = rnorm / rnorm0
       rate = red**(1.0_dp/itr)
       write(unit=*,fmt='(i3,a,es14.6,es13.3,f8.3)') itr, ':', rnorm, red, rate
 
-      if (rnorm < 1.0e-6 * rnorm0) exit
+      if (rnorm < TOL * rnorm0) exit
     end do
 
     sol = reshape(u, shape(sol))
 
   end subroutine solve
-  
-  function residual (sys, u) result (r)
-  
-    type(system),  intent(inout) :: sys
-    real(kind=dp), intent(in)    :: u(0:,0:)
-    real(kind=dp) :: r(sys%nx,sys%ny)
-    
+
+  subroutine residual (sys, u, r)
+
+    type(system), intent(inout) :: sys
+    real(dp), intent(in) :: u(0:,0:)  ! solution array padded with ghost cells
+    real(dp), intent(out) :: r(sys%nx,sys%ny)
+
     integer :: j, k
-    
+
     call update_system (sys, u)
-    
+
     do k = 1, sys%ny
       do j = 1, sys%nx
         r(j,k) = sys%ac(j,k) * u(j,k) - sys%ax(j,k) * u(j-1,k) - sys%ax(j+1,k) * u(j+1,k) &
@@ -132,52 +156,55 @@ contains
                                       - sys%q(j,k)
       end do
     end do
-    
-  end function residual
-  
-  subroutine update_system (sys, u)
-  
-    type(system),  intent(inout) :: sys
-    real(kind=dp), intent(in)    :: u(0:,0:)
-  
+
+  end subroutine residual
+
+  subroutine update_system (sys, upad)
+
+    type(system), intent(inout) :: sys
+    real(dp), intent(in) :: upad(0:,0:) ! solution vector padded with ghost cells
+
     integer :: j, k
-    real(kind=dp) :: t
-    
+    real(dp) :: t, rx, ry
+
+    rx = sys%hx / sys%hy
+    ry = sys%hy / sys%hx
+
     sys%ax = 0.0_dp
     sys%ay = 0.0_dp
     do k = 1, sys%ny
       do j = 1, sys%nx
-        t = 1.0_dp / (sys%a + u(j,k))
-        sys%ax(j,k)   = sys%ax(j,k)   + t
-        sys%ax(j+1,k) = sys%ax(j+1,k) + t
-        sys%ay(j,k)   = sys%ay(j,k)   + t
-        sys%ay(j,k+1) = sys%ay(j,k+1) + t
+        t = 1.0_dp / (sys%a + upad(j,k))
+        sys%ax(j,k)   = sys%ax(j,k)   + rx*t
+        sys%ax(j+1,k) = sys%ax(j+1,k) + rx*t
+        sys%ay(j,k)   = sys%ay(j,k)   + ry*t
+        sys%ay(j,k+1) = sys%ay(j,k+1) + ry*t
       end do
     end do
-    
+
     sys%ax = 2.0_dp / sys%ax
     sys%ay = 2.0_dp / sys%ay
-    
+
     do k = 1, sys%ny
       do j = 1, sys%nx
         sys%ac(j,k) = sys%ax(j,k) + sys%ax(j+1,k) + sys%ay(j,k) + sys%ay(j,k+1)
       end do
     end do
-    
+
   end subroutine update_system
-  
-  function ssor (sys, nsweep, omega, r) result (z)
-  
-    type(system),  intent(in) :: sys
-    integer,       intent(in) :: nsweep
-    real(kind=dp), intent(in) :: omega
-    real(kind=dp), intent(in) :: r(0:,0:)
-    real(kind=dp) :: z(0:sys%nx+1,0:sys%ny+1)
-    
+
+  subroutine ssor_pc (sys, nsweep, omega, r)
+
+    type(system), intent(in)    :: sys
+    integer,      intent(in)    :: nsweep
+    real(dp),     intent(in)    :: omega
+    real(dp),     intent(inout) :: r(sys%nx,sys%ny)
+
     integer :: i, j, k
-    
-    z = r
-    
+    real(dp) :: z(0:sys%nx+1,0:sys%ny+1)
+
+    z = 0.0_dp
+
     do i = 1, nsweep
       !! Forward sweep
       do k = 1, sys%ny
@@ -196,13 +223,15 @@ contains
         end do
       end do
     end do
-    
-  end function ssor
-    
-  function norm (x) result (normx)
-    real(kind=dp), intent(in) :: x(:,:)
-    real(kind=dp) :: normx
-    normx = sqrt(sum(x**2))
-  end function norm
 
-end program nka_unit_test
+    !! Copy result to return array.
+    r = z(1:sys%nx,1:sys%ny)
+
+  end subroutine ssor_pc
+
+  real(dp) function l2norm (x)
+    real(dp), intent(in) :: x(:)
+    l2norm = sqrt(sum(x**2))
+  end function l2norm
+
+end program nka_f95_test
